ec0b32fc746f78cee3005c5afb008aefcc7bd3c2
Hilda Amponsah <hilda.amp@hotmail.com>
2020-07-02 12:17:53 +0100

    adding comments

--- directory.rb
+++ directory.rb
@@ -1,16 +1,20 @@
-puts "The students of Villains Academy"
-puts "--------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have "
+#First, we print the list of students
+puts 'The students of Villains Academy'
+puts '--------------------'
+puts 'Dr. Hannibal Lecter'
+puts 'Darth Vader'
+puts 'Nurse Ratched'
+puts 'Michael Corleone'
+puts 'Alex DeLarge'
+puts 'The Wicked Witch of the West'
+puts 'Terminator'
+puts 'Freddy Krueger'
+puts 'The Joker'
+puts 'Joffrey Baratheon'
+puts 'Norman Bates'
+print "line1\nline2\nline3" #ensuring it is in double quotes 
+#Finally, we print the total number of students
+print 'Overall, we have '
+#It's important that print() doesn't add new characters
 print 11
-puts " great students"
+puts ' great students'
