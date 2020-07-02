a48e31b8f313228c9a3b13ffcd4bcd5c226ecfc8
Hilda Amponsah <hilda.amp@hotmail.com>
2020-07-02 16:02:31 +0100

    refactoring further

--- directory.rb
+++ directory.rb
@@ -18,12 +18,12 @@ def print_header
   puts "--------------------"
 end
 
-def print(names)
-  names.each do |name|
-   puts name
- end
-end
-
+# def print(names)
+#   names.each do |name|
+#    puts name
+#  end
+# end
+#
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
@@ -33,21 +33,29 @@ def print(students)
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have  #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
-#Finally, we print the total number of students
-# puts "Overall, we have #{students.count} great students"
-# puts students[0]
-puts students[1]
-# puts students[2]
-# puts students[3]
-# puts students[4]
-# puts students[5]
-# puts students[6]
-# puts students[7]
-# puts students[8]
-# puts students[9]
-# puts students[10]
-#It's important that print() doesn't add new characters
