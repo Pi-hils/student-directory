d46c40a01430c03011842f4ad04087e4c4fd80a4
Hilda Amponsah <hilda.amp@hotmail.com>
2020-07-07 07:40:27 +0100

    Adding a file

--- directory.rb
+++ directory.rb
@@ -8,10 +8,14 @@ students = [
     {name: "Alex DeLarge", cohort: :november},
     {name: "The Wicked Witch of the West", cohort: :november},
     {name: "Freddy Krueger", cohort: :november},
+    {name: "Terminator", cohort: :november},
     {name: "The Joker", cohort: :november},
     {name: "Joffrey Barathon", cohort: :november},
     {name: "Norman Bates", cohort: :november}
 ]
+
+# create an empty array accessible to all methods
+@students = []
 
 def print_header
   puts "The students of Villains Academy"
@@ -38,24 +42,34 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have  #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have  #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
-#nothing happens until we call the methods
-students = input_students
+@students = input_students
 print_header
-print(students)
+# print(students)
 print_footer(students)
+
+
+def save_students
+  #open the file for writinng
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+     student_date = [student[:name], student[:cohort]]
+     csv_line = student_data.join(",")
+     file.puts csv_line
+   end
+  file.close
+end
